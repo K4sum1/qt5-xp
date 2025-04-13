f04eb9fb960b06e08ceb69ced3acf239947573a3
K4sum1 <k4sum1@eclipse.cx>
2025-04-13 09:16:16 -0500

    Remove qtwebengine as I'm not backporting Chromium

--- qtwebengine/src/3rdparty/chromium/third_party/webgpu-cts/src/src/demo/a.spec.ts
+++
@@ -1,6 +0,0 @@
-export const description = 'Description for a.spec.ts';
-
-import { makeTestGroup } from '../common/framework/test_group.js';
-import { UnitTest } from '../unittests/unit_test.js';
-
-export const g = makeTestGroup(UnitTest);
--- qtwebengine/src/3rdparty/chromium/third_party/webgpu-cts/src/src/webgpu/idl/README.txt
+++
@@ -1,2 +0,0 @@
-Tests to check that the WebGPU IDL is correctly implemented, for examples that objects exposed
-exactly the correct members, and that methods throw when passed incomplete dictionaries.
